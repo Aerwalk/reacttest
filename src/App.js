52f577369c778d4af1b4ee618ccfabbc06973e2b
Aerwalk <kedr64rus@gmail.com>
2024-03-12 10:10:45 +0400

    123456

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>BB full REACT</p>
-        <p> REACT</p>
         <p>CUCUMBERS</p>
         <p>QQQQQQQQQQQ</p>
         <a
